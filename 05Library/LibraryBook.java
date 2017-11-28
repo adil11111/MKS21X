f906a7a840b8558afb9d53000d6c27ba2a110eb3
Adil Gondal <agondal@stuy.edu>
2017-11-28 14:44:12 -0500

    Made and added LibraryBook file

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,26 @@
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+    public LibraryBook(String authorName, String titleName, String number,String callNumber){
+	super(authorName,titleName,number);
+	this.callNumber=callNumber;
+    }
+    /* public static void main(String[] args){
+	LibraryBook boop=new LibraryBook ("Rowling"," Harry"," 12345","caller");
+	System.out.println(boop.callNumber);
+	}*/
+    public String getCallNumber(){
+	return callNumber;
+    }
+    public void setCallNumber(String callNumber){
+	this.callNumber=callNumber;
+    }
+    abstract void checkout(String patron, String due);
+    abstract void returned();
+    abstract String circulationStatus();
+    public int compareTo(String otherCallNumber){
+	return compareTo(otherCallNumber);
+    }
+    public String toString(){
+	return super.toString()+ " Circulation Status: " + circulationStatus() + " Call Number: " + callNumber;
+    }
+}
