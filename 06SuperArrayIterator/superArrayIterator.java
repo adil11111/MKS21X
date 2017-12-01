3cf02ea0d2ca67dcbf3423980b6ed65f55a7a3f5
Adil Gondal <Adil@Adils-MBP.home>
2017-12-01 03:35:37 -0500

    Finished the next() and hasNext() methods

--- 06SuperArrayIterator/superArrayIterator.java
+++ 06SuperArrayIterator/superArrayIterator.java
@@ -1,16 +1,22 @@
 import java.util.Iterator;
 public class superArrayIterator implements Iterator<String>{
-    private SuperArray[] data;
+    private SuperArray data;
     private int current;
-    public superArrayIterator(SuperArray[] data){
+    public superArrayIterator(SuperArray data){
 	current=0;
 	this.data=data;
     }
     public String next(){
-	return null;
+	if (hasNext()){
+	    current++;
+	}
+	else{
+	    System.exit(0);
+	}
+	return data.get(current-1);
     }
     public boolean hasNext(){
-	return true;
+	return current<= data.size();
     }
 }
 	
